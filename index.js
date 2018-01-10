3722301ee99aac86db92dff947d99c92c190748b
Tom Xu <tomxu89@gmail.com>
2018-01-10 23:08:15 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -3,8 +3,12 @@ function happyHolidays () {
   return "Happy olidays!"
 }
 
-function happyHolidayTo (name) {
+function happyHolidaysTo (name) {
   return `Happy holidays, ${name}!`
+}
+
+function happyHolidayTo (holiday, name) {
+  `Happy ${holiday}, ${name}!`
 }
 
 function holidayCountdown (holiday, days) {
